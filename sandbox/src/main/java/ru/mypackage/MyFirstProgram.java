d7745a220126c130a00e207b6bd5527461116743
ups2201 <ups2201@gmail.com>
2020-11-19 14:15:36 +0300

    add 2 class

--- sandbox/src/main/java/ru/mypackage/MyFirstProgram.java
+++ sandbox/src/main/java/ru/mypackage/MyFirstProgram.java
@@ -2,6 +2,25 @@ package ru.mypackage;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, world");
+		hello("user");
+		hello("alexey");
+
+		Square s = new Square(5);
+		System.out.println(area(s));
+
+		Rectangle r = new Rectangle(4,6);
+		System.out.println(area(r));
+	}
+
+	public static void hello(String text) {
+		System.out.println("Hello, " + text);
+	}
+
+	public static double area(Square s){
+		return s.l*s.l;
+	}
+
+	public static double area(Rectangle r){
+		return r.a*r.b;
 	}
 }
---
+++ sandbox/src/main/java/ru/mypackage/Square.java
@@ -0,0 +1,9 @@
+package ru.mypackage;
+
+public class Square {
+	public double l;
+
+	public Square(double l){
+		this.l = l;
+	}
+}
