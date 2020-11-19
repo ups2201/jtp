674eadfb9a7a77cc38e5e880040853d425ef756a
ups2201 <ups2201@gmail.com>
2020-11-19 09:21:58 +0300

    codeStyle

--- sandbox/src/main/java/ru/mypackage/MyFirstProgram.java
+++ sandbox/src/main/java/ru/mypackage/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package ru.mypackage;
 
 public class MyFirstProgram {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello, world");
 	}
 }
